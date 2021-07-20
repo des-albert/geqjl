e944d272bf7e71b57633033b2e3c028609e145e4
Albert <des.albert@gmail.com>
2021-07-20 13:10:34 -0700

    code update

--- bound_matrix.jl
+++ bound_matrix.jl
@@ -1,5 +1,4 @@
 function bound_matrix!()
-
     ar = 2 / Mm1
     r0 = 1 / alpha
     az = sh*ar
@@ -21,7 +20,7 @@ function bound_matrix!()
     for j in Mr:j2:Mr
         ip[kp] = j + 2
         ip[kp + 1] = j + Mm1
-        kp = kp + 2
+        kp += 2
     end
     kp = kp + 1
 
@@ -64,6 +63,34 @@ function bound_matrix!()
     end
 
     # Matrix elements
- 
+
+    arh = ar/2.
+    azh = az/2.
+
+    for i in 1:kp
+
+        nl = 1
+        for j in 1:m2
+            aux[nl, i] = flux(rt[i], rt[nl], zt[i] - zt[nl])/(sh*rt[nl])
+            aux[nl + 1, i] = flux(rt[i], rt[nl + 1], zt[i] - zt[nl + 1])/(sh*rt[nl + 1])
+            nl += 2
+        end
+
+        for j in 1:n2
+            aux[nl, i] = flux(rt[i], rt[nl], zt[i] - zt[nl])*sh/(rt[nl] + arh)
+            aux[nl + 1, i] = flux(rt[i], rt[nl + 1], zt[i] - zt[nl + 1])*sh/(rt[nl + 1] - arh)
+            nl += 2
+        end
+
+    end
+
     return nothing
+end
+
+function flux(rv, rst, zv)
+
+    ak = 4.0*rv*rst/((rv + rst)^2 + zv^2)
+    x = ((rv - rst)^2 + zv^2)/((rv + rst)^2 + zv^2)
+    return sqrt(rv*rst/ak)*((1 - ak/2.)*ellipk(x^2) - ellipe(x^2))/pi
+
 end
--- solver.jl
+++ solver.jl
@@ -1,5 +1,6 @@
 using Base: Float64
 using DelimitedFiles
+using SpecialFunctions
 
 include("bound_matrix.jl")
 
@@ -10,14 +11,14 @@ Nz = Mr
 MN = Mr * Nz
 Mm1 = Mr - 1
 Nm1 = Nz - 1
-llp = 2*(Mr + Nz) - 8 
+llp = 2 * (Mr + Nz) - 8
 
 Rmin, Rmax, Zmin, Zmax = readdlm("solver.dat")
 
-dr = (Rmax - Rmin)/Mm1
-dz = (Zmax - Zmin)/Nm1
-alpha = (Rmax - Rmin)/(Rmax + Rmin)
-sh = dz/dr
+dr = (Rmax - Rmin) / Mm1
+dz = (Zmax - Zmin) / Nm1
+alpha = (Rmax - Rmin) / (Rmax + Rmin)
+sh = dz / dr
 ss = sh^2
 
 println("Rmin = ", Rmin, " Rmax = ", Rmax, " dr = ", dr)
@@ -26,5 +27,8 @@ println("alpha = ", alpha, " sh = ", sh, " ss = ", ss)
 
 ip = Array{Int}(undef, llp)
 jp = Array{Int}(undef, llp)
+aux = Array{Float64}(undef, llp, llp)
 
 bound_matrix!()
+
+println(aux[22, 32])
